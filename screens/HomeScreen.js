19b105a7fa4bf2f79b11443b2db9dafb624e5b25
Pakopac <lilian.pacaud@supinternet>
2019-10-12 13:22:23 +0200

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,28 @@
+import * as WebBrowser from 'expo-web-browser';
+import React from 'react';
+import {
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      
+    </View>
+  );
+}
+
+HomeScreen.navigationOptions = {
+  header: null,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  }
+});
